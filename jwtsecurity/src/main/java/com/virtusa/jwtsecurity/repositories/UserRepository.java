5d41f31d0603bb82279b5bfc3297fbc20abcb379
kavithasar <85490976+kavithasar@users.noreply.github.com>
2021-06-07 12:40:19 +0100

    Delete jwtsecurity directory

--- jwtsecurity/src/main/java/com/virtusa/jwtsecurity/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.jwtsecurity.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtusa.jwtsecurity.models.User;
-
-public interface UserRepository extends JpaRepository<User,String>{
-
-}
