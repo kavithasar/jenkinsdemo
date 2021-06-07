0fab55fa8bcf09892bad8436b221ff5b491e6e5f
kavithasar <85490976+kavithasar@users.noreply.github.com>
2021-06-07 12:38:30 +0100

    Delete food-order-publisher-rabbit-with-stream directory

--- food-order-publisher-rabbit-with-stream/README.md
+++
@@ -1,2 +0,0 @@
-# food-order-publisher
-Code written for the blog post: http://e4developer.com/2018/01/28/setting-up-rabbitmq-with-spring-cloud-stream/
--- food-order-publisher-rabbit-with-stream/src/main/java/com/e4developer/foodorderpublisher/FoodOrderPublisher.java
+++
@@ -1,7 +0,0 @@
-package com.e4developer.foodorderpublisher;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(FoodOrderSource.class)
-public class FoodOrderPublisher {
-}
