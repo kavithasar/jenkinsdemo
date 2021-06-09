387a63c0d6467296114e6859e63d4e63b4df049c
kavithasar <85490976+kavithasar@users.noreply.github.com>
2021-06-09 08:52:16 +0100

    Add files via upload

---
+++ uploaddocumentapi/src/main/java/com/hsbc/banking/UploaddocumentapiApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaddocumentapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploaddocumentapiApplication.class, args);
+	}
+
+}
---
+++ uploaddocumentapi/src/test/java/com/hsbc/banking/UploaddocumentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaddocumentapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
